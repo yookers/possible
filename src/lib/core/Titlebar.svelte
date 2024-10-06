76208bcc27af755ee124cdd4861fc8f8e2f67cbd
yookers <yowokers@pm.me>
2024-10-06 10:38:00 -0400

    feat: custom titlebar test

---
+++ src/lib/core/Titlebar.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+    import type { Window } from '@tauri-apps/api/window';
+    import { onDestroy, onMount } from 'svelte';
+    import RiCheckboxBlankLine from 'svelte-remixicon/RiCheckboxBlankLine.svelte';
+    import RiCheckboxMultipleBlankLine from 'svelte-remixicon/RiCheckboxMultipleBlankLine.svelte';
+    import RiCloseLine from 'svelte-remixicon/RiCloseLine.svelte';
+    import RiSubtractLine from 'svelte-remixicon/RiSubtractLine.svelte';
+
+    // @ts-ignore: Does not detect __TAURI__.window
+    const { getCurrentWindow } = window.__TAURI__.window;
+
+    const appWindow: Window = getCurrentWindow();
+
+    let isMaximized = $state(false);
+    let unlisten: () => void;
+
+    async function updateIsMaximized() {
+        isMaximized = await appWindow.isMaximized();
+    }
+
+    onMount(async () => {
+        unlisten = await appWindow.onResized(updateIsMaximized);
+    });
+
+    onDestroy(() => {
+        unlisten();
+    });
+</script>
+
+<div
+    data-tauri-drag-region
+    class="fixed left-0 top-0 z-50 flex h-10 w-full select-none items-center justify-between border-b border-[#26264c] bg-[#1a1a2e]"
+>
+    <div class="flex h-full items-center px-4">
+        <p data-tauri-drag-region class="text-sm font-medium">Possible</p>
+    </div>
+
+    <div class="flex h-full items-center">
+        <button
+            class="flex h-full w-12 items-center justify-center hover:bg-slate-600"
+            onclick={() => appWindow.minimize()}
+        >
+            <RiSubtractLine class="size-5" />
+        </button>
+        <button
+            class="flex h-full w-12 items-center justify-center hover:bg-slate-600"
+            onclick={() => appWindow.toggleMaximize()}
+        >
+            {#if isMaximized}
+                <RiCheckboxMultipleBlankLine class="size-3.5" />
+            {:else}
+                <RiCheckboxBlankLine class="size-3.5" />
+            {/if}
+        </button>
+        <button
+            class="flex h-full w-12 items-center justify-center hover:bg-red-600"
+            onclick={() => appWindow.close()}
+        >
+            <RiCloseLine class="size-5" />
+        </button>
+    </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
     import type { Snippet } from 'svelte';
+    import Titlebar from '$lib/core/Titlebar.svelte';
 
     import '../app.css';
 
     let { children }: { children: Snippet } = $props();
 </script>
 
-{@render children()}
+<Titlebar />
+<main class="h-dvh overflow-auto pl-16 pr-4 pt-10">
+    {@render children()}
+</main>
